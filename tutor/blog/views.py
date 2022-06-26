1a9a1f4c1b1a9f25cc1b247bb37a20a552829dda
mcz91 <macczeka@gmail.com>
2022-06-26 12:35:21 +0200

    dzien dobry 2606

--- tutor/blog/views.py
+++ tutor/blog/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from asyncio import QueueEmpty
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+from blog.models import Choice, Question 
+from django.utils import timezone
 
-from django.http import HttpResponse
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'blog/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'blog/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'blog/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('blog:results', args=(question.id,)))
